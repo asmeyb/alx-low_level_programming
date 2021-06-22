56fd3e26a2bbdfeb0019cb0b32c82021f9cd1222
asmeyb <asmeb@gmail.com>
2021-06-22 05:23:47 -0700

    6-abs Edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,21 @@
 #include "holberton.h"
+
 /**
-* print_sign - Determines if the input number
-* greater, iqual or less than zero.
+* _abs - gives the absolute value
+* of an integer.
 *
-* @n: The input number as an integer.
+* @n: input number as an integer.
 *
-* Return: 1 is greater than zero. 0 is zero.
-* -1 is less than zero.
+* Return: absolute value
 */
-int print_sign(int n)
-{
-	if (n > 0)
-		{
-		_putchar(43);
-	return (1);
-		}
-	else if (n < 0)
-		{
-		_putchar(45);
-	return (-1);
-		}
+int _abs(int n)
+	{
+	if (n >= 0)
+	{
+	return (n);
+	}
 	else
-		{
-		_putchar(48);
-	return (0);
-		}
-		_putchar('\n');
+	{
+	return (n * -1);
+	}
 }
