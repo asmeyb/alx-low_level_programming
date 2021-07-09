a987d76f2bb0251315faac393ca0cd7c799d0e05
asmeyb <asmeb@gmail.com>
2021-07-09 09:26:44 -0700

    All Update

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
+#ifndef HOLBERTON
+#define HOLBERTON
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +20,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
 
-#endif /* _HOLBERTON_H_ */
+#define NULL 0
