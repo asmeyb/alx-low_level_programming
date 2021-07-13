225c984baec703aa886f760a3618e0bbfbb564f6
asmeyb <asmeb@gmail.com>
2021-07-13 08:37:26 -0700

    Update on 100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,57 +1,42 @@
+include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
-int length(int ac, char **av);
-
 /**
- * argstostr -  Concatenates all the arguments of your program.
- * @ac: Counter for arguments
- * @av: Array of values
- * Return: A pointer to a string
- */
+ * *argstostr - concatenates all arguements to the program
+ * @ac: arguement count
+ * @av: pointer to arguements
+ * Return: pointer to new space in memory or null
+ **/
 char *argstostr(int ac, char **av)
 {
-	int len;
-	int i;
-	int x;
-	int j;
-	char *heap_array;
+	char *strDup;
+	int i, j, k, size;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	len = length(ac, av);
-	heap_array = malloc(len + 1);
-
-	for (x = 0, i = 0; i < ac; i++, x++)
+	size = 0;
+/* count the number of chars in each string */
+	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++, x++)
-		{
-			heap_array[x] = av[i][j];
-		}
-		heap_array[x] = '\n';
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
 	}
-	heap_array[x] = '\0';
-	return (heap_array);
-}
-
-/**
- * length -  Calcualtes the length
- * @ac: Counter for arguments
- * @av: Array of values
- * Return: A the total amount of characters on the string
+	size++;
+/* allocate memory for total number of chars and
+ * new line for each word
  */
-int length(int ac, char **av)
-{
-	int i;
-	int j;
-	int counter;
-
-	counter = 0;
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			counter++;
+			strDup[k++] = av[i][j];
 		}
+		strDup[k++] = '\n';
 	}
-	return (counter + ac);
+	strDup[k] = '\0';
+	return (strDup);
 }
