32ec14aaac70703a8a50aa9c41b221d0833676f3
asmeyb <asmeb@gmail.com>
2021-07-13 04:11:34 -0700

    Change on 100- argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,57 +1,42 @@
-#include <stdlib.h>
 #include <stdio.h>
-int length(int ac, char **av);
-
+#include <stdlib.h>
+#include "holberton.h"
 /**
- * argstostr -  Concatenates all the arguments of your program.
- * @ac: Counter for arguments
- * @av: Array of values
- * Return: A pointer to a string
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	int len;
-	int i;
-	int x;
-	int j;
-	char *heap_array;
+	int i, n, k = 0, len = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	len = length(ac, av);
-	heap_array = malloc(len + 1);
 
-	for (x = 0, i = 0; i < ac; i++, x++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++, x++)
-		{
-			heap_array[x] = av[i][j];
-		}
-		heap_array[x] = '\n';
-	}
-	heap_array[x] = '\0';
-	return (heap_array);
-}
-
-/**
- * length -  Calcualtes the length
- * @ac: Counter for arguments
- * @av: Array of values
- * Return: A the total amount of characters on the string
- */
-int length(int ac, char **av)
-{
-	int i;
-	int j;
-	int counter;
-
-	counter = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
 		{
-			counter++;
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
 		}
 	}
-	return (counter + ac);
+	return (str);
 }
