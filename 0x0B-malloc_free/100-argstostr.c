7aff3c9fc225767a837a8d1af13ca9ff94cca075
asmeyb <asmeb@gmail.com>
2021-07-13 03:34:58 -0700

    Change

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -40,16 +40,3 @@ char *argstostr(int ac, char **av)
 	}
 	return (str);
 }
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-}
