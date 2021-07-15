507417ce98708ea0daf5a33e40bb9d4050d59af5
asmeyb <asmeb@gmail.com>
2021-07-15 12:52:04 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,39 +1,21 @@
-#include "holberton.h"
 #include <stdlib.h>
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *strDup;
-	int i;
-	unsigned int j;
+	unsigned int i;
+	char *memory;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
-	if (strDup == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		strDup[i] = s1[i];
-		i++;
-	}
-	while (j < n && s2[j] != '\0')
-	{
-		strDup[i] = s2[j];
-		i++, j++;
-	}
-	strDup[i] = '\0';
-	return (strDup);
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*(memory + i) = 0;
+	return ((void *)memory);
 }
