86d5f3d4ee4739f3fcf4c44ee43d7d4396932324
asmeyb <asmeb@gmail.com>
2021-07-15 11:36:12 -0700

    Update on All

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,35 +1,59 @@
 #include <stdlib.h>
 
 /**
- * _realloc - Creates new space in memory, copies the contents over, fre
- * @ptr: Pointer to previously allocated array
- * @old_size: Size of old array
- * @new_size: Size of new array
- * Return: Pointer to a void
+ * _memcpy - Copy n bytes from memory area src to memory area dest
+ * @dest: Memory area to copy to
+ * @src: Memory area to copy from
+ * @n: Amount to copy from memory area
+ *
+ * Return: Pointer to area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _realloc - Reallocate a memory block using malloc
+ * @ptr: Old memory block
+ * @old_size: Size of of old memory block
+ * @new_size: Size the new memory block should be
+ *
+ * Return: Pointer to new memory space, NULL if it fails
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i;
-	char *pt;
-	char *heap_array;
+	void *nptr;
+	unsigned int min;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		return (nptr);
+	}
+	if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	/*Casting a void* to a char* */
-	pt = (char *)ptr;
-	heap_array = malloc(sizeof(char) * new_size);
-	if (heap_array == NULL)
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size)
+		min = new_size;
+	else
+		min = old_size;
+	nptr = malloc(new_size);
+	if (nptr == NULL)
 		return (NULL);
-	if (ptr != NULL)
-	{
-		for (i = 0; i < old_size; i++)
-			heap_array[i] = pt[i];
-	}
+	nptr = _memcpy(nptr, ptr, min);
 	free(ptr);
-	return (heap_array);
+	return (nptr);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,50 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-int length(int min, int max);
-
 /**
- * array_range - Allocates and writes an array and all the values needed
- * @min: the starting point
- * @max: the ending point
- * Return: Pointer to the array
+ * array_range - Create array of integers filled with numbers from min to max
+ * @min: First number in the array
+ * @max: Largest number in the array
+ *
+ * Return: Pointer to new array, NULL if failed
  */
 int *array_range(int min, int max)
 {
-	int i;
-	int len;
-	int *heap_array;
+	int *ar;
+	int i, size, n;
 
 	if (min > max)
 		return (NULL);
-
-	len = length(min, max);
-
-	heap_array = malloc(sizeof(int) * len);
-	if (heap_array == NULL)
-		return (NULL);
-
-	for (i = 0; min <= max ; i++)
+	size = max - min;
+	ar = malloc((size + 1) * sizeof(int));
+	i = 0;
+	n = min;
+	while (n <= max)
 	{
-		heap_array[i] = min;
-		min++;
+		ar[i] = n;
+		i++;
+		n++;
 	}
-	return (heap_array);
-}
-
-/**
- * length - Calculates the length of the array
- * @min: the starting point
- * @max: the ending point
- * Return: The total length for the array
- */
-int length(int min, int max)
-{
-	int l;
-
-	for (l = 0; min != max; min++)
-	{
-		l++;
-	}
-	l++;
-	return (l);
+	return (ar);
 }
