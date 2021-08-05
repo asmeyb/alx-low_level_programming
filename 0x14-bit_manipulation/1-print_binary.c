fe9b291043254968aa223cfe47583e1ccc34d393
asmeyb <asmeb@gmail.com>
2021-08-05 01:51:52 -0700

    All in One

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,38 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_binary - decimal to binary without use / %
- * @n: the decimal
- * Description: convert decimal to binary
- * section header: the header of this function is holberton.h
- * Return: no return
+ * binary_to_uint - Converters a binary number to an unsigned integer
+ * @b: String representing a binary number
+ * Return: The converted number or 0 if b is NULL or if there are chars
+ * that are not 0 or 1
  */
-void print_binary(unsigned long int n)
+unsigned int binary_to_uint(const char *b)
 {
-	int flag = 0;
-	unsigned long int mask = 1;
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+	unsigned int sum;
+	unsigned int pow;
+	int base;
 
-	mask <<= 63;
-	if (n == 0)
-		_putchar('0');
-
-	while (mask > 0)
+	base = 2;
+	sum = 0;
+	pow = 1;
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
+		return ((b[0] - 48));
+	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
 	{
-		if ((n & mask) == 0 && flag == 1)
-			_putchar('0');
-		if ((n & mask) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-
-		mask = mask >> 1;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
+		len--;
+		pow = 1; /*Reseting the exponent*/
 	}
+	return (sum);
 }
