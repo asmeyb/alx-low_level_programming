b7c58979de6455f90d1866eeb0deebfd585d005b
asmeyb <asmeb@gmail.com>
2022-01-18 23:56:56 -0800

    Remaining

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,77 +1,43 @@
 #include "search_algos.h"
-
 /**
- * advanced_binary - basic binary search does not necessarily
- * return the index of the first value in the array (if this
- * value appears more than once in the array). In this method,
- * i’ll have to solve this problem.
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- * @value: the value to search for
- * Return:  the index where value is located
- * assuming that array will be sorted in ascending order
- * If value is not present in array or if array is NULL,
- * your function must return -1
+ * advanced_binary - search for a number in a binary way
+ * @array: the input
+ * @size: the size of the input
+ * @value: the value to seaach
+ * Return: the index of the value, or -1 when the values is not present
  */
-
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL)
 		return (-1);
-
-	return (binary_search_ex(array, value, 0, (int)size - 1));
+	return (helperBinary(array, 0, size - 1, value));
 }
-
-
 /**
- * binary_search_ex -  searches for a value in a sorted array
- * of integers using the Binary search algorithm but it's
- * a helper for the advanced_binary_search algorithm
- * @array: pointer to the first element of the array to search in
- * @value:  the value to search for
- * @first: start index of the subarray
- * @last: last index of the subarray
- * Return:  the index where value is located
- * You can assume that array will be sorted in ascending order
- * You can assume that value won’t appear more than once in array
- * If value is not present in array or if array is NULL, your
- * function must return -1
- * int binary_search(int *array, size_t size, int value)
+ * helperBinary - search for a number in a binary way
+ * @array: the input
+ * @s: the begining
+ * @e: the end
+ * @value: the value to seaach
+ * Return: the index of the value, or -1 when the values is not present
  */
-
-int binary_search_ex(int *array, int value, int first, int last)
+int helperBinary(int *array, int s, int e, int value)
 {
-	int i, mid;
-	int last_idx = last;
-	int first_idx = first;
+	int m, i;
 
-	if (array == NULL)
-		return (-1);
-
-	while (first_idx <= last_idx)
+	printf("Searching in array: ");
+	for (i = s; i < e; i++)
 	{
-		printf("Searching in array:");
-		for (i = first_idx; i <= last_idx; i++)
-		{
-			printf(" %i", array[i]);
-			if (i != last_idx)
-				printf(",");
-		}
-		printf("\n");
-
-		mid = (int)((first_idx + last_idx) / 2);
-
-		if (value > array[mid])
-			first_idx = mid + 1;
-		else if (value < array[mid])
-			last_idx = mid - 1;
-		else
-		{
-			if (value == array[mid - 1])
-				return (binary_search_ex(array, value, first_idx, mid));
-			else
-				return (mid);
-		}
+		printf("%i, ", *(array + i));
 	}
+	printf("%i\n", *(array + i));
+	m = ((e - s) / 2) + s;
+	if (*(array + m) == value && *(array + (m - 1)) != value)
+		return (m);
+	if (s == e)
+		return (-1);
+	if (*(array + m) >= value)
+		return (helperBinary(array, s, m, value));
+	if (*(array + m) < value)
+		return (helperBinary(array, m + 1, e, value));
 	return (-1);
 }
